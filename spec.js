b04b6d2f98779ffec78f35593dd846d546f0da83
Stanley <lims95@yahoo.com>
2020-04-23 19:20:42 -0400

    added sqlize

--- spec.js
+++ spec.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db_test');
+
+const User = conn.define('user', {
+    name: Sequelize.STRING
+});
+
 const { expect } = require('chai');
 
 const _app = require('supertest')(app);
 
-app.get('/api/users', (req, res, next) => {
-    res.send([ {name: "lucy"} ]);
+app.get('/api/users', async(req, res, next) => {
+    res.send(await User.findAll());
 });
 
 describe('the truth', () => {
@@ -15,9 +22,16 @@ describe('the truth', () => {
 });
 
 describe('GET /api/users', () => {
+    beforeEach(async() => {
+        await conn.sync({ force: true });
+        await Promise.all([
+            User.create({ name: "lucy" }),
+            User.create({ name: "moe" })
+        ]);     
+    });
     it('returns the users', async () => {
         const response = await _app.get('/api/users');
         expect(response.status).to.equal(200);
-        expect(response.body.length).to.equal(1);
+        expect(response.body.length).to.equal(2);
     }); 
 });
